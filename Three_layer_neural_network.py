6edca2a7c59d7d288e70317f17655678a37140f2
wanghaowms <37298585+wanghaowms@users.noreply.github.com>
2019-03-31 23:54:13 +0800

    Update Three_layer_neural_network.py

--- Three_layer_neural_network.py
+++ Three_layer_neural_network.py
@@ -7,7 +7,11 @@
 
 import numpy as np
 def sigmoid(x):#softmax函数
-    return 1 / (1 + np.exp(-x))
+    c=np.max(x)
+    exp_a=np.exp(x-c)
+    sum_exp_a=np.sum(exp_a)
+    y=exp_a/sum_exp_a
+    return y
 def identity_function(x):#恒等函数
  return x
 def init_network():
